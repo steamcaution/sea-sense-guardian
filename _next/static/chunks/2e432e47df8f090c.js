28949a3f06417d1e0a7bf9851516d0cb4da46763
steamcaution <steamcaution@users.noreply.github.com>
2025-12-10 13:57:26 +0000

    deploy: 6b20546631ae3ca69a4d4909c7b3d8677ec29cf5

--- _next/static/chunks/2e432e47df8f090c.js
+++ _next/static/chunks/2e432e47df8f090c.js
@@ -1,7 +1,7 @@
 __turbopack_load_page_chunks__("/_app", [
   "static/chunks/1f1f932863090e6c.js",
   "static/chunks/8a240a74dd7eef9a.js",
-  "static/chunks/08c3cceb5fa38971.js",
+  "static/chunks/da7b4e169992e626.js",
   "static/chunks/111eec09a2153dd0.css",
-  "static/chunks/turbopack-2b058e09918d7229.js"
+  "static/chunks/turbopack-3fd6de1a2d862d27.js"
 ])
